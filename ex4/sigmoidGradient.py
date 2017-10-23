571f204191401463928ebe0d6d3e0e6487052741
panjw@esrichina.com.cn <panjw@esrichina.com.cn>
2017-10-23 10:23:57 +0800

    normalization

---
+++ ex4/sigmoidGradient.py
@@ -0,0 +1,9 @@
+#-*-coding:utf-8 -*-
+__author__ = 'Jy2881'
+
+import numpy as np
+import sigmoid
+
+def sigmoidGradient(z):
+    g = np.zeros(np.shape(z))
+    g = sigmoid.sigmoid(z)*(1.0-sigmoid.sigmoid(z))
